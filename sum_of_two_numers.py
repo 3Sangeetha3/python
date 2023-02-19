9022972497a47919429a118d2ab46442de37a5ad
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-02-19 20:58:13 +0530

    Delete sum_of_two_numers.py

--- sum_of_two_numers.py
+++
@@ -1,5 +0,0 @@
-a = input("Enter the value of a: ")    # input is used take input from the user
-b = input("Enter the value of b: ")    # I am getting input from user as string 
-sum = int(a) + int(b)   #int (a) is a type conversion here I am converting string from the user to integer int(a)+int(b) is called concatanation
-print(sum)
-print(type(sum))
