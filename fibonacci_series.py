95ef817a0eee95fd54407e18cd967a6f47869ab4
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-25 14:24:46 +0530

    fibonacci series

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,12 +1,16 @@
 #Calculate Fibonacci series one number less than the given input x, and also calculate the sum of all alternate numbers (Even-numbered) in the resultant list. 
-x = int(input('x: '))
-a, b = 0, 1
-total_sum = 0
-i = 0
-while a < x:
+n = int(input("n: "))
+
+#initialize variables
+a = 0
+b = 1
+
+#iterate until a is less than or equal to n
+while a <= n:
+    #print a
     print(a)
-    if i % 2 == 0:
-        total_sum += a
-    a, b = b, a + b
-    i += 1
-print(f'sum: {total_sum}')
+    #calculate next number in fibonacci series
+    c = a + b
+    #update a and b
+    a = b
+    b = c
